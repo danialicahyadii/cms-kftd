9ab9177f1cfb3d9bb65d7dae03148f14eff26b9e
danialicahyadi <danialicahyadii@gmail.com>
2024-10-29 08:16:56 +0700

    hide search input

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,14 +2,11 @@
 <nav class="navbar navbar-expand-lg main-navbar">
     <form class="form-inline mr-auto">
         <ul class="navbar-nav mr-3">
-            <li><a href="#"
-                    data-toggle="sidebar"
-                    class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
-            <li><a href="#"
-                    data-toggle="search"
-                    class="nav-link nav-link-lg d-sm-none"><i class="fas fa-search"></i></a></li>
+            <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
+            {{-- <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i
+                        class="fas fa-search"></i></a></li> --}}
         </ul>
-        <div class="search-element">
+        {{-- <div class="search-element">
             <input class="form-control"
                 type="search"
                 placeholder="Search"
@@ -87,15 +84,12 @@
                     </a>
                 </div>
             </div>
-        </div>
+        </div> --}}
     </form>
     <ul class="navbar-nav navbar-right">
-        <li class="dropdown"><a href="#"
-                data-toggle="dropdown"
+        <li class="dropdown"><a href="#" data-toggle="dropdown"
                 class="nav-link dropdown-toggle nav-link-lg nav-link-user">
-                <img alt="image"
-                    src="{{ asset('img/avatar/avatar-1.png') }}"
-                    class="rounded-circle mr-1">
+                <img alt="image" src="{{ asset('img/avatar/avatar-1.png') }}" class="rounded-circle mr-1">
                 <div class="d-sm-none d-lg-inline-block">Hi, {{ Auth::user()->name }}</div>
             </a>
             <div class="dropdown-menu dropdown-menu-right">
@@ -114,7 +108,7 @@
                 </a> --}}
                 {{-- <div class="dropdown-divider"></div> --}}
                 <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
-                class="dropdown-item has-icon text-danger">
+                    class="dropdown-item has-icon text-danger">
                     <i class="fas fa-sign-out-alt"></i> Logout
                 </a>
                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
